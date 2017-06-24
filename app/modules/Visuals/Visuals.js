c568b6db2fe057c5759ffd267fa76d54751350eb
Roland <rdorde@gmail.com>
2017-06-24 21:50:32 +0200

    feat(Grid): add

--- app/modules/Visuals/Visuals.js
+++
@@ -1,6 +0,0 @@
-jQuery(function ($) { 
-  // var scene = document.getElementById('ro-Visual');
-  // if(scene) var parallax = new Parallax(scene);
-
-  // var clipboard = new Clipboard('.ro-Visual');
-});
