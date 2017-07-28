c8df0cd6c60d408fcfd7882e7659a370c13463b1
Roland <rdorde@gmail.com>
2017-07-28 17:09:15 +0200

    fix(Visual): position on tablet

--- app/modules/Visual/Visual.js
+++ app/modules/Visual/Visual.js
@@ -3,4 +3,10 @@ jQuery(function ($) {
   if(scene) var parallax = new Parallax(scene);
 
   // var clipboard = new Clipboard('.ro-Visual');
+  // var header = $('.ro-Hero-header', '.ro-Hero--availability');
+
+  // $('.ro-Visual-contact').hover(
+  //   () => header.css('opacity', 0.3),
+  //   () => header.css('opacity', 1)
+  //   );
 });
