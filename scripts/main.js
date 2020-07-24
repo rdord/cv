9d2d5d584c4a30df048c0bf1ff0a45b316963532
Roland Đorđević <roland.djordjevic@ineor.si>
2020-07-24 14:35:49 +0200

    beautify code

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,39 @@
-"use strict";jQuery(document).ready(function(e){function t(e){if(e.matches&&l){new Parallax(l)}else{new Parallax(a),new Parallax(i),new Parallax(o)}}var n=window.matchMedia("(min-width: 1280px)"),l=document.getElementById("ro-Visual"),a=document.getElementById("ro-Circle--1"),i=document.getElementById("ro-Circle--2"),o=document.getElementById("ro-Circle--3");t(n),n.addListener(t);var c=void 0,r=!1,d=document.getElementsByTagName("body")[0];e(window).scroll(function(){r||(r=!0,d.classList.add("is-scrolling")),clearTimeout(c),c=setTimeout(function(){d.classList.remove("is-scrolling"),r=!1},70)})});
+jQuery(document).ready(function ($) {
+  // Initialize parallax on different element regarding the screen size
+  const mediaQuery = window.matchMedia('(min-width: 1280px)');
+  const visual = document.getElementById('ro-Visual');
+  const circle1 = document.getElementById('ro-Circle--1');
+  const circle2 = document.getElementById('ro-Circle--2');
+  const circle3 = document.getElementById('ro-Circle--3');
+
+  function initParallax(mediaQuery) {
+    if (mediaQuery.matches && visual) {
+      let parallax = new Parallax(visual);
+    } else {
+      let parallax2 = new Parallax(circle1);
+      let parallax3 = new Parallax(circle2);
+      let parallax4 = new Parallax(circle3);
+    }
+  }
+
+  initParallax(mediaQuery);
+  mediaQuery.addListener(initParallax);
+
+  // Add class to the body on scroll event
+  let timer;
+  let flag = false;
+  let body = document.getElementsByTagName('body')[0];
+
+  $(window).scroll(() => {
+    if (!flag) {
+      flag = true;
+      body.classList.add('is-scrolling');
+    }
+
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      body.classList.remove('is-scrolling');
+      flag = false;
+    }, 70);
+  });
+});
